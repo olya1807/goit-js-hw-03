3b02cca8e659396ffda8869c7267e8f6cb52bd89
Olha Skrypkina <skrypkina.olha@gmail.com>
2020-10-11 18:14:12 +0300

    ht3

---
+++ js/task3.js
@@ -0,0 +1,45 @@
+// Задача 3-3
+// Поиск наибольшего значения среди свойств объекта
+// Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников и возвращает имя самого продуктивного (который выполнил больше всех задач). Сотрудники и кол-во выполненных задач содержатся как свойства объекта в формате "имя":"кол-во задач".
+
+const findBestEmployee = function (employees) {
+  "use strict";
+  // Write code under this line
+  let maxTasks = 0;
+  let bestEmployee = "";
+  for (const employee in employees) {
+    const numberOfTasks = employees[employee];
+    if (numberOfTasks > maxTasks) {
+      maxTasks = numberOfTasks;
+      bestEmployee = employee;
+    }
+  }
+  return bestEmployee;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  ann: 29,
+  david: 35,
+  helen: 1,
+  lorence: 99,
+};
+console.log(findBestEmployee(developers));
+// 'lorence'
+
+const supports = {
+  poly: 12,
+  mango: 17,
+  ajax: 4,
+};
+console.log(findBestEmployee(supports));
+// 'mango'
+
+const sellers = {
+  lux: 147,
+  david: 21,
+  kiwi: 19,
+  chelsy: 38,
+};
+console.log(findBestEmployee(sellers));
+// 'lux'
